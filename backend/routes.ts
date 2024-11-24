2dce5479bcbaf110f411f3593212fd464a308aa4
Tarun Tirkey <taruntirkey91@live.com>
2024-11-24 17:00:59 +0530

    Separate routes file

---
+++ backend/routes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const apiRouter = express.Router();
+
+/**
+ * Combing all routes in this file.
+ * apiRouter.use("/module/v1", moduleRouter);
+ */
+
+export default apiRouter;
